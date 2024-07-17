f0789d686d13457c568bf1d49e4c17cf69b60111
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-07-17 18:42:42 +0100

    Update open_file.c

--- exection/open_file.c
+++ exection/open_file.c
@@ -6,22 +6,22 @@
 /*   By: yozainan <yozainan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 17:20:21 by elchakir          #+#    #+#             */
-/*   Updated: 2024/07/16 10:28:27 by yozainan         ###   ########.fr       */
+/*   Updated: 2024/07/17 17:56:42 by yozainan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void redirection_in_out(t_data *data)
+void redirection_in_out(t_data *data, Command *cmd)
 {
     Redirection *redir;
 
-    redir = data->cmd->input;
+    redir = cmd->input;
     while (redir != NULL)
     {
         //close
-        data->cmd->fdin = open(redir->filename, O_RDONLY);
-        if (data->cmd->fdin == -1)
+        cmd->fdin = open(redir->filename, O_RDONLY);
+        if (cmd->fdin == -1)
         {
             ft_putstr_fd("minishell: ", 2);
             ft_putstr_fd(redir->filename, 2);
@@ -32,11 +32,11 @@ void redirection_in_out(t_data *data)
         }
         redir = redir->next;
     }
-    redir = data->cmd->output;
+    redir = cmd->output;
     while (redir != NULL)
     {
-        data->cmd->fdout = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (data->cmd->fdout == -1)
+        cmd->fdout = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (cmd->fdout == -1)
         {
             ft_putstr_fd("minishell: ", 2);
             ft_putstr_fd(redir->filename, 2);
@@ -47,11 +47,11 @@ void redirection_in_out(t_data *data)
         }
         redir = redir->next;
     }
-    redir = data->cmd->append_output;
+    redir = cmd->append_output;
     while (redir != NULL)
     {
-        data->cmd->fdout = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        if (data->cmd->fdout == -1)
+        cmd->fdout = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (cmd->fdout == -1)
         {
             ft_putstr_fd("minishell: ", 2);
             ft_putstr_fd(redir->filename, 2);
@@ -72,7 +72,7 @@ void open_check_redirections(t_data *data)
     current_cmd = data->cmd;
     while (current_cmd != NULL)
     {
-        redirection_in_out(data);
+        redirection_in_out(data, current_cmd);
         current_cmd = current_cmd->next;
     }
 }
